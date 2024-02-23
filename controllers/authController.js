5945a64e0fb769f30e7bfd809596c84974137737
adershradhakrishnan <adershradhakrishnan07@gmail.com>
2024-02-23 16:00:52 +0530

    saa message

---
+++ controllers/authController.js
@@ -0,0 +1,77 @@
+let success_function=require('../utils/response-handler').success_function;
+let error_function=require('../utils/response-handler').error_function;
+
+
+
+
+exports.login = async function(req,res){
+
+    try{
+        let email = req.body.email;
+        let password = req.body.password;
+
+        if (email&&password){
+            let user = await users.findOne({
+                $and : [{email:email }],
+            })
+
+            if(!user){
+                let response = error_function({"status" :400,"message" : "Invalid email"});
+                res.status(response.statusCode).send(response);
+                return;
+            }
+
+            if(user){
+                bcrypt.compare(password,user.password,async (error,auth)=>{
+                    if(auth === true) {
+                        let access_token = jwt.sign(
+                            {user_id : user_id},
+                            process.env.PRIVATE_KEY,
+                            {expiresIn: "10d"}
+                        );
+                        let response = success_function({
+                            status:200,
+                            data:access_token,
+                            message: "Login successful"
+                        });
+                        response.user_type = user_type;
+                        res.status(response.statusCode).send(response);
+                        return;
+                    } else{
+                        let response = error_function({
+                            status:401,
+                            message: "invalid credentials"
+                        });
+                        res.status(response.statusCode).send(response);
+                        return;
+                    }
+                });
+            }else{
+                let response = error_function({
+                    status:401,
+                    message: "invalid credentials"
+                });
+                res.status(response.statusCode).send(response);
+                return;
+            }
+
+        }else{
+            if(!email){
+                let response=error_function({
+                    status:422,
+                    message: "email is required"
+                });
+                res.status(response.statusCode).send(response);
+                return;
+            }
+
+            if(!password){
+                let response=success_function({
+                    status422,
+                    message: "password required"
+                });
+                res
+            }
+        }
+    }
+}
