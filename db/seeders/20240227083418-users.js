0a5a1c7fc5e4903731e3b0176fe04258e682caa6
adershradhakrishnan <adershradhakrishnan07@gmail.com>
2024-03-13 15:57:32 +0530

    erwe message

--- db/seeders/20240227083418-users.js
+++ db/seeders/20240227083418-users.js
@@ -13,7 +13,8 @@ module.exports = {
             phonenumber: "7034603650",
             pincode: "683575",
             
-            password: "$2a$04$Fz.QmkBnszB/Lbj7SNoV3ukl/n2gxxzHLrnsCMr8BPBBht0V7huei" //123456789
+            password: "$2a$04$Fz.QmkBnszB/Lbj7SNoV3ukl/n2gxxzHLrnsCMr8BPBBht0V7huei",//123456789
+            user_type: "65e9987a550c5c1f798ca45b"
            }
           ])
         },
